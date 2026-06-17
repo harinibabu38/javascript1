1d008879b24731365a47bf36e70cb12fd15c3fcf
harini c <harinibabu38@gmail.com>
2026-06-17 22:18:37 +0530

    added code

--- test.js
+++ test.js
@@ -1,1 +1,10 @@
-created test.js file
+let numbers = [10, 20, 30, 40, 50];
+console.log("Array elements:");
+for (let i = 0; i < numbers.length; i++) {
+    console.log(numbers[i]);
+}
+let sum = 0;
+for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+}
+console.log("Sum =", sum);
